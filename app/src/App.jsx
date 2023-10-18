5d4eb309089ee0bfa65b4623f9c47e6b6a94e2bf
Anmol Thakur <anmolll.thakurrr@gmail.com>
2023-10-18 20:00:30 +0530

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,169 @@
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import SearchResults from "./Components/SearchResults";
+export const BASE_URL = "http://localhost:9000"
+
+const App = () => {
+
+  const [data, setData]=useState(null);
+  const [loading, setLoading]=useState(false);
+  const [error, setError]=useState(null);
+  const [filteredData, setFilteredData] = useState(null);
+  const [selectedBtn, setSelectedBtn] = useState("all");
+
+  useEffect(()=>{
+    const fetchFoodData = async ()=>{
+      try {
+        const response = await fetch(BASE_URL);
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+        setFilteredData(json);
+      } catch (error) {
+        setError("Unable to fetch data");
+      }
+    };
+    fetchFoodData();
+  },[]);
+
+
+  const searchFood=(e)=>{
+    const searchValue = e.target.value;
+    console.log(searchValue);
+    if(searchValue === ""){
+      setFilteredData(null);
+    }
+    const filter = data?.filter((food)=>
+      food.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filter);
+    
+
+
+
+  }
+
+  const filteredFood = (type) =>{
+    if(type==="all"){
+      setFilteredData(data);
+      setSelectedBtn("all");
+      return;
+    }
+
+    const filter = data?.filter((food)=>
+      food.type.toLowerCase().includes(type.toLowerCase())
+    );
+    setFilteredData(filter);
+    setSelectedBtn(type);
+ }
+  
+   
+  const filterBtns = [
+    {
+    name:"All",
+    type:"all",
+  },
+  {
+    name:"Breakfast",
+    type:"breakfast",
+  },
+  {
+    name:"Lunch",
+    type:"lunch",
+  },
+  {
+    name:"Dinner",
+    type:"dinner",
+  },
+
+  ]
+
+
+  if(error) return <div>{error}</div>
+  if(loading) return <div>Loading...</div>
+
+
+
+  return (
+    <Container>
+      <TopContainer>
+        <div className="logo">
+          <img src="/Foody Zone.png" alt="logo" />
+        </div>
+
+        <div className="search">
+          <input onChange={searchFood} type="text" placeholder="Search Food..." />
+        </div>
+      </TopContainer>
+
+      <FilterContainer>
+        {
+          filterBtns.map((value)=>(
+            <Button
+            isSelected={selectedBtn===value.type}            
+            key={value.name} onClick={()=>filteredFood(value.type)}>{value.name}</Button>
+          ))
+        }
+      </FilterContainer>
+
+      <SearchResults data={filteredData}/>
+
+      
+    </Container>
+  );
+};
+
+export default App;
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const TopContainer = styled.section`
+  height: 140px;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  align-items: center;
+
+  .search Input {
+    background-color: transparent;
+    border: 1px solid red;
+    color: white;
+    border-radius: 5px;
+    font-size: 16px;
+    height: 40px;
+    padding: 0 10px;
+    &::placeholder{
+      color:white;
+    }
+  }
+@media (0 < width <600px){
+  flex-direction :column ;
+  height: 120px;
+}
+`;
+
+
+
+
+const FilterContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  padding-bottom: 40px;
+
+`;
+export const Button = styled.button`
+  padding: 6px 12px;
+  border-radius: 5px;
+  background: ${({isSelected}) => (isSelected ? "lightgreen" : "#ff4343")};
+  outline: 1px solid ${({isSelected}) => (isSelected ? "white" : "#ff4343")};
+  
+  border: none;
+  color: white;
+  cursor: pointer;
+  &:hover{
+    background-color: #f60404;
+  }
+`;
---
+++ app/src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    background-color: #323334;
+    color: white;
+    min-height: 100vh;
+    font-family: 'Inter', sans-serif;;
+  }
+
+
+
+`;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+  </React.StrictMode>
+);
